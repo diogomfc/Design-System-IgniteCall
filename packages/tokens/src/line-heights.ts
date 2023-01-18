a11033b0bc47525d19f3e7533eef775280821ed9
diogo silva <diogosilva@iMac.local>
2023-01-18 16:50:04 -0300

    Configuração do eslint Monorepo

--- packages/tokens/src/line-heights.ts
+++ packages/tokens/src/line-heights.ts
@@ -1,5 +1,5 @@
 export const lineHeights = {
-  shorter: '125%', 
+  shorter: '125%',
   short: '140%',
   base: '160%',
   tall: '180%',
--- packages/tokens/src/space.ts
+++ packages/tokens/src/space.ts
@@ -3,7 +3,7 @@ export const space = {
   2: '0.5rem', // 8px
   3: '0.75rem', // 12px
   4: '1rem', // 16px
-  5: '1.25rem',   // 20px
+  5: '1.25rem', // 20px
   6: '1.5rem', // 24px
   7: '1.75rem', // 28px
   8: '2rem', // 32px
